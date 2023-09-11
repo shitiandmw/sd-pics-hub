593b5749bfec2c3b277352c1350c7fa50d7463e4
shitian <284485094@qq.com>
2023-09-11 17:20:27 +0800

    完善数字分身创建页面

---
+++ client/src/components/svg/svg-activetab.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg
+    t="1693540700196"
+    class="w-full h-full"
+    style="fill: currentColor"
+    viewBox="0 0 1027 1024"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    p-id="7312"
+  >
+    <path
+      d="M75.471 948.549h873.057v-873.1l-873.057 873.1z m535.412-115.385l-39.459-39.457-103.8-103.773 39.458-39.428 103.771 103.742 206.8-206.773 39.459 39.459-246.229 246.23z"
+      p-id="7313"
+    ></path>
+  </svg>
+</template>
+
+<script>
+export default {};
+</script>
+
