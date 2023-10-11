e8d10b721c66a557c01629e05be2ecd122444714
shitian <lushitian502@gmail.com>
2023-10-11 15:44:01 +0800

    修改任务完成界面

--- client/src/pages/guide/step.vue
+++ client/src/pages/guide/step.vue
@@ -1,53 +1,71 @@
 <template>
-  <phone-container class="h-full ">
+  <phone-container class="h-full overflow-auto">
     <div class="p-6 h-full flex flex-col">
       <steps
         :step_index="step_index"
         @switchStep="switchStep"
         class="-mt-2"
       ></steps>
-      <div class="flex-1 overflow-hidden">
-        <scroll-view class="h-full" scroll-y="true">
-          <!-- 1.选择模板 -->
-
-          <div class="" v-if="step_index == 0">
+      <div class="flex-1" :class="step_index == 0 ? 'overflow-hidden' : ''">
+        <!-- 1.选择模板 -->
+        <scroll-view class="h-full" scroll-y="true" v-if="step_index == 0">
+          <div class="">
             <div class="mt-8 text-2xl mb-1">写真模板</div>
             <div class="text-base text-gray-500 mb-2">
               选择一个你喜欢的写真模板，出片风格会与此模板相似
             </div>
             <div class="py-4 -mx-6 px-6">
-              <templates class="" @select="selectTemplates" ></templates>
+              <templates class="" @select="selectTemplates"></templates>
             </div>
-          </div>
-          <!-- 2.制作数字分身 -->
-          <div v-if="step_index == 1">
-            <div class="mt-8 text-2xl mb-1">制作数字分身</div>
-            <div class="text-base text-gray-500 mb-6">
-              制作一个你的数字分身，用于代替你在虚拟世界拍摄写真
-            </div>
-            <addDoppelganger></addDoppelganger>
-          </div>
-
-           <!-- 3.完成 -->
-           <div v-if="step_index == 2">
-            <div class="mt-32 mb-10 flex items-center justify-center">
-               <div class="w-24 h-24 text-green-500">
-                  <svgSuccess></svgSuccess>
-               </div>
-            </div>
-            <div class="text-lg text-gray-600 mb-6 text-center">
-              任务处理中，您可以先看看其他的模板
-            </div>
-
-            <lbutton class="shadow-xl mt-24 ">去逛逛</lbutton>
           </div>
         </scroll-view>
+
+        <!-- 2.制作数字分身 -->
+        <div v-if="step_index == 1">
+          <div class="mt-8 text-2xl mb-1">制作数字分身</div>
+          <div class="text-base text-gray-500 mb-6">
+            制作一个你的数字分身，用于代替你在虚拟世界拍摄写真
+          </div>
+          <addDoppelganger></addDoppelganger>
+          <div class="h-10"></div>
+        </div>
+
+        <!-- 3.完成 -->
+        <div v-if="step_index == 2">
+          <!-- <div class="mt-32 mb-10 flex items-center justify-center">
+            <div class="w-24 h-24 text-green-500">
+              <svgSuccess></svgSuccess>
+            </div>
+          </div> -->
+          <div class="text-lg mt-10">
+            <div class="flex items-start">
+              <!-- <div class="w-12 h-12 text-gray-900 mr-2 -mt-2">
+                <svgSuccess></svgSuccess>
+              </div> -->
+              <div class="space-y-2">
+                <div class="text-2xl flex items-center text-gray-900">
+                  任务创建成功
+                </div>
+                <div class="text-base text-gray-600">
+                  写真任务已经成功提交到系统，AI处理任务需要一点时间，请等待...
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="flex  mt-14 space-x-4">
+            <lbutton class="flex-1 h-10 rounded-full text-base">查看任务状态</lbutton>
+            <lbutton class="border h-10 w-24 rounded-full text-base bg-gray-50 text-gray-600 ">去首页</lbutton>
+          </div>
+        </div>
       </div>
     </div>
     <lpopup v-model="template.show" v-if="step_index == 0" class="">
       <div class="flex flex-col h-full">
         <div class="pb-6 pt-2 px-6 overflow-y-auto flex-1">
-          <template-detail :id="template._id" v-if="template.show"></template-detail>
+          <template-detail
+            :id="template._id"
+            v-if="template.show"
+          ></template-detail>
         </div>
         <div class="h-20 w-full px-6 py-2 translate-y-1">
           <lbutton class="shadow-xl">使用此模板</lbutton>
@@ -71,17 +89,22 @@ import addDoppelganger from "../components/addDoppelganger.vue";
 export default {
   data() {
     return {
-      step_index: 1,
+      step_index: 2,
       template: {
         _id: "",
         show: false,
       },
 
-      subdata:{
-        select_template_id:"",
-        select_doppelganger_id:"",
-      }
+      subdata: {
+        select_template_id: "",
+        select_doppelganger_id: "",
+      },
 
+      sub_result: {
+        success: false,
+        error: "",
+        data: {},
+      },
     };
   },
   onLoad() {},
@@ -89,7 +112,7 @@ export default {
     switchStep: function (index) {
       this.step_index = index;
     },
-    selectTemplates(id){
+    selectTemplates(id) {
       this.template._id = id;
       this.template.show = true;
     },
